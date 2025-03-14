2ed05d6a5a1a0a3d9d9925937b587c40fac9e91e
bUzR4Ks <aulia.bilibili@gmail.com>
2025-03-14 08:55:25 +0700

    initial code

---
+++ laravel-cookies-app/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::get('/salam', function () {
+    // Coba ambil nama dari session atau cookie
+    $name = session('name', request()->cookie('name', 'Guest'));
+    return view('salam', compact('name'));
+});
+
+Route::post('/submit-name', function (Request $request) {
+    $name = $request->input('name');
+    // Simpan nama ke session dan cookie
+    session(['name' => $name]);
+    return redirect('/salam')->withCookie(cookie('name', $name, 60));
+});
